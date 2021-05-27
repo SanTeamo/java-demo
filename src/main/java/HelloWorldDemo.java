fe3d97cb93c6c6eff94c17d725a4b7250d86293e
SanTeAmo <834713675@qq.com>
2021-05-27 09:30:37 +0800

    init

---
+++ src/main/java/HelloWorldDemo.java
@@ -0,0 +1,13 @@
+/**
+ * Title: <br>
+ * Description: <br>
+ * Date: 2021/5/27 <br>
+ *
+ * @author SanTeAmo
+ * @version 1.0
+ */
+public class HelloWorldDemo {
+    public static void main(String[] args) {
+        System.out.println("Hello,world!");
+    }
+}
